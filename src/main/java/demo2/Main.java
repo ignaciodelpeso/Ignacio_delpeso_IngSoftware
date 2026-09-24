2c0e2da0414e1f31f7448d39f4ceae4699283ff8
Ignacion del Peso <nachodelpeso@gmail.com>
2026-09-24 18:07:49 +0200

    cambio mensaje de ingles a español

--- src/main/java/demo2/Main.java
+++ src/main/java/demo2/Main.java
@@ -2,6 +2,8 @@ package demo2;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Hola Mundo");
+
+        
     }
 }
